c429a779ee03aec37defeb0aa417a55d381cef7e
Kononnable <kononnable@gmail.com>
2026-05-27 14:49:17 +0200

    Remove unused re-export of LifecycleState and State from cluster module

--- client/src/cluster.rs
+++ client/src/cluster.rs
@@ -5,5 +5,3 @@ mod options;
 pub use controller::ClusterController;
 pub use error::ClusterOptionsValidationError;
 pub use options::ClusterOptions;
-
-pub use crate::io_loop::{LifecycleState, State};
